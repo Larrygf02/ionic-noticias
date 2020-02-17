19e1d78f2c975dcdebf67c9edbe36db0ffbc8779
larrygf02 <raulgf_02@hotmail.com>
2020-02-16 22:37:37 -0500

    Ion segment nav-bar

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
-
+export class Tab2Page implements OnInit{
+  @ViewChild(IonSegment, { static: true }) segment: IonSegment;
   constructor() {}
+  categorias = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
 
+  ngOnInit() {
+    //Inicializa el segment en la primera categoria
+    this.segment.value = this.categorias[0];
+  }
 }
