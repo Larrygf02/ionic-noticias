7b6013b611c31e230b0f4e553664c03b03221d9d
larrygf02 <raulgf_02@hotmail.com>
2020-02-16 23:14:03 -0500

    Mostrando noticias por categoria

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { ThrowStmt } from '@angular/compiler';
 import { NoticiasService } from '../../services/noticias.service';
+import { Article } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
@@ -12,13 +13,23 @@ export class Tab2Page implements OnInit{
   @ViewChild(IonSegment, { static: true }) segment: IonSegment;
   constructor( private noticiasService: NoticiasService) {}
   categorias = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
-
+  noticias: Article[] = [];
   ngOnInit() {
     //Inicializa el segment en la primera categoria
-    this.segment.value = this.categorias[0];
-    this.noticiasService.getTopHeadLinesCategoria(this.categorias[0])
+    this.segment.value = this.categorias[0]
+    this.cargarNoticias(this.segment.value)
+  }
+
+  cambioCategoria(event) {
+    this.noticias = []
+    this.cargarNoticias(event.detail.value);
+  }
+
+  cargarNoticias(categoria: string) {
+    this.segment.value = categoria;
+    this.noticiasService.getTopHeadLinesCategoria(categoria)
                         .subscribe( resp => {
-                          
+                           this.noticias.push(...resp.articles) 
                         })
   }
 }
