7ef80334301cd0d63268c9d3aa9862878e2b77b1
larrygf02 <raulgf_02@hotmail.com>
2020-02-17 22:17:58 -0500

    Ultimos detalles de la app noticias

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Article } from '../interfaces/interfaces';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,18 @@ import { Article } from '../interfaces/interfaces';
 export class DataLocalService {
 
   noticias: Article[] = [];
-  constructor(private storage: Storage) {
+  constructor(private storage: Storage,
+    public toastController: ToastController) {
     this.cargarFavoritos()
    }
 
+  async presentToast( message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 1500
+    });
+    toast.present()
+  }
   guardarNoticia( noticia: Article){
     console.log(noticia)
     const existe = this.noticias.find( noti => noti.title === noticia.title)
@@ -20,11 +29,13 @@ export class DataLocalService {
       this.storage.set('favoritos', this.noticias )
     }
     console.log(this.noticias)
+    this.presentToast(' Agregado a Favoritos')
   }
 
   borrarNoticia (noticia: Article) {
     this.noticias = this.noticias.filter(noti => noti.title !== noticia.title)
     this.storage.set('favoritos', this.noticias )
+    this.presentToast(' Eliminado de Favoritos')
   }
 
   async cargarFavoritos() {
